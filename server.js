0a4cfda4f20b661e1f9a78ee367e58a9288771a8
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-21 21:55:54 -0400

    added fs.writeFile to server.js

--- server.js
+++ server.js
@@ -22,11 +22,44 @@ app.get(`/api/notes`, (req, res) => {
     console.info(`${req.method} request received to get notes`);
 });
 
+app.post(`/api/notes`, (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        const reviewString = JSON.stringify(newNote);
+
+        fs.writeFile(`./db/${newNote.title}.json`,
+        reviewString, (err) =>
+        err
+        ? console.error(err)
+        : console.log(
+            `Note for ${newNote.title} has been written to JSON file`
+        ));
+
+        const response = {
+            status: `success`,
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(`Error in posting note`);
+    }
+});
 
 
 
 
 
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => 
+    console.log(`listening on port ${PORT}`)
+);
 
