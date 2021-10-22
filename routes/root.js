a320616003742bd65ef7c6b95846c78bae31c64a
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-21 23:09:44 -0400

    made some modifications to root.js file

---
+++ routes/root.js
@@ -0,0 +1,21 @@
+import express from `express`;
+import {fileURLToPath} from `url`;
+import path from `path`; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicFolderRootObj = {root: path.join(__dirname, `../public`)}
+
+
+const router = express.Router()
+
+router.get(`/`, function(req, res){
+    res.sendFile(`index.html`, publicFolderRootObj);
+});
+
+router.get(`/notes`, function(req, res){
+    res.sendFile(`notes.html`, publicFolderRootObj);
+});
+
+export default router
+
